eee12bcfb3f9df01b528570bffba2e1d990cc13f
Carl Rizk <carlrizk88@gmail.com>
2023-03-30 15:39:15 +0200

    move loader testing code to loader file

--- loader.py
+++ loader.py
@@ -44,3 +44,34 @@ def __load_agent(agent_json: Dict, items: List[Item]) -> Tuple[str, Preferences]
             )
 
     return (name, preferences)
+
+
+if __name__ == "__main__":
+    environment = load_json("./environment.json")
+
+    print("Items:")
+    for item in environment[0]:
+        print("-", item)
+
+    print()
+
+    print("Agents:")
+    for agent in environment[1]:
+        print("", "Name:", agent[0])
+        print("", "Preferences:")
+        preferences = agent[1]
+        print("", "", "Criterion Order:")
+        for criterion_name in preferences.get_criterion_name_list():
+            print("", "", "", "-", criterion_name)
+        print("", "", "Criterion Values:")
+        for criterion_value in preferences.get_criterion_value_list():
+            print(
+                "",
+                "",
+                "",
+                "-",
+                f"{criterion_value.get_item().get_name(): <10}",
+                f"{criterion_value.get_criterion_name(): <40}",
+                criterion_value.get_value(),
+            )
+        print()
