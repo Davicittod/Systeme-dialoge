83b53a91a90365790a7f9260d4cfab911d4569de
Davicittod <davidhera96@gmail.com>
2023-03-31 09:18:49 +0200

    Typing and gitignore mod

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -1,32 +1,39 @@
+from typing import List
+
 from mesa import Model
-from mesa . time import RandomActivation
+from mesa.time import RandomActivation
 
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
+from preferences.Item import Item
+from preferences.Preferences import Preferences
 
 
-class ArgumentAgent (CommunicatingAgent) :
-    """ ArgumentAgent which inherit from CommunicatingAgent .
-    """
-    def __init__(self, unique_id, model, name, preferences) :
+class ArgumentAgent(CommunicatingAgent):
+    """ArgumentAgent which inherit from CommunicatingAgent ."""
+
+    def __init__(
+        self, unique_id: int, model: Model, name: str, preferences: Preferences
+    ):
         super().__init__(unique_id, model, name, preferences)
-        self.preference = preferences
+        self.preference: Preferences = preferences
 
     def step(self):
         super().step()
 
-    def get_preference(self):
+    def get_preference(self) -> Preferences:
         return self.preference
 
-    def generate_preferences(self,List_items) :
+    def generate_preferences(self, item_list: List[Item]):
         # see question 3
         # To be completed
         pass
 
+
 class ArgumentModel(Model):
-    """ ArgumentModel which inherit from Model .
-    """
-    def __init__(self) :
+    """ArgumentModel which inherit from Model ."""
+
+    def __init__(self):
         self.schedule = RandomActivation(self)
         self.__messages_service = MessageService(self.schedule)
         # To be completed
@@ -41,6 +48,7 @@ class ArgumentModel(Model):
         self.__messages_service.dispatch_messages()
         self.schedule.step()
 
-if __name__ == " __main__ " :
-        argument_model = ArgumentModel ()
-    # To be completed
+
+if __name__ == " __main__ ":
+    argument_model = ArgumentModel()
+# To be completed
