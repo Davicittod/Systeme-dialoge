50883e74e6fdd7313ab8291d2a34677580a9aaf9
Carl Rizk <carlrizk88@gmail.com>
2023-04-14 10:32:27 +0200

    Implement Argument Class

--- arguments/Argument.py
+++ arguments/Argument.py
@@ -1,5 +1,10 @@
+from typing import List
+
 from arguments.Comparison import Comparison
 from arguments.CoupleValue import CoupleValue
+from preferences.CriterionName import CriterionName
+from preferences.Item import Item
+from preferences.Value import Value
 
 
 class Argument:
@@ -13,14 +18,27 @@ class Argument:
     couple_values_list :
     """
 
-    def __init__(self, boolean_decision, item):
+    def __init__(self, boolean_decision: bool, item: Item):
         """Creates a new Argument ."""
-        # To be completed
+        self.boolean_decision = boolean_decision
+        self.item = item
+        self.comparison_list: List[Comparison] = []
+        self.couple_values_list: List[CoupleValue] = []
 
-    def add_premiss_comparison(self, criterion_name_1, criterion_name_2):
+    def add_premiss_comparison(
+        self, best_criterion_name: CriterionName, worst_criterion_name: CriterionName
+    ):
         """Adds a premiss comparison in the comparison list ."""
-        # To be completed
+        self.comparison_list.append(
+            Comparison(best_criterion_name, worst_criterion_name)
+        )
 
-    def add_premiss_couple_values(self, criterion_name, value):
+    def add_premiss_couple_values(self, criterion_name: CriterionName, value: Value):
         """Add a premiss couple values in the couple values list ."""
-        # To be completed
+        self.couple_values_list.append(CoupleValue(criterion_name, value))
+
+    def __str__(self) -> str:
+        neg = "" if self.boolean_decision else "not"
+        couple_values = ", ".join(map(str, self.couple_values_list))
+        comparaisons = ", ".join(map(str, self.comparison_list))
+        return f"{neg} {self.item.get_name()} <= {couple_values}, {comparaisons}"
--- test_arguments.py
+++ test_arguments.py
@@ -1,11 +1,18 @@
+from arguments.Argument import Argument
 from arguments.Comparison import Comparison
 from arguments.CoupleValue import CoupleValue
 from preferences.CriterionName import CriterionName
+from preferences.Item import Item
 from preferences.Value import Value
 
 if __name__ == "__main__":
-    comp = Comparison(CriterionName.CONSUMPTION, CriterionName.DURABILITY)
-    print(comp)
+    comparaison = Comparison(CriterionName.CONSUMPTION, CriterionName.DURABILITY)
+    print(comparaison)
 
     couple_value = CoupleValue(CriterionName.CONSUMPTION, Value.BAD)
     print(couple_value)
+
+    argument = Argument(False, Item("Item", "Description"))
+    argument.add_premiss_comparison(CriterionName.CONSUMPTION, CriterionName.DURABILITY)
+    argument.add_premiss_couple_values(CriterionName.CONSUMPTION, Value.BAD)
+    print(argument)
