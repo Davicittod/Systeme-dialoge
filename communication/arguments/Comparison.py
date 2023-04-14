999fb68f7b9ff776ef34edfd189ceffd4d528aa5
Davicittod <davidhera96@gmail.com>
2023-04-14 09:51:03 +0200

    Arguments folder created

---
+++ communication/arguments/Comparison.py
@@ -0,0 +1,17 @@
+
+class Comparison :
+    """ Comparison class .
+    This class implements a comparison object used in argument object .
+
+    attr :
+    b est_criterion_name :
+    w o rst_criterion_name :
+    """
+
+    def __init__(self, best_criterion_name, worst_criterion_name):
+        """ Creates a new comparison .
+        """
+        # To be completed
+    
+    def __str__(self): 
+        pass
